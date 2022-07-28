7fb9b26376aae5b734855e07b381f5ee13e87ced
Tatheer Mehdi <93590787+codethrx@users.noreply.github.com>
2022-07-28 23:05:47 +0500

    2-Express app created

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
-console.log("Server app");
+//IMPORTS
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 5000;
+//express app
+const app = express();
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  console.log(`Pathname--${req.path}`);
+  console.log(`Method--${req.method}`);
+  next();
+});
+
+//
+//Listening to requests.
+app.listen(PORT, () => {
+  console.log(`Listening to port#${PORT}`);
+});
